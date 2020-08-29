75257b078b540cce66fd8c6565ef0dc93584c435
AidanMcB <aidankmcbride@gmail.com>
2020-08-28 23:05:30 -0400

    fully functional, needs styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,54 +2,56 @@ import React from 'react';
 // Components
 import Screen from './Screen'
 
-export default class Calculator extends React.Component{
+export default class Calculator extends React.Component {
 
-    
-    render(){
-        console.log(this.props)
-        return(
-            <div>
-            <br />
-            <Screen display={this.props.display}/>
-            <div className="row1">
-            <button name="(">(</button>
-            <button name=")">)</button>
-            <button name="">*</button>
-            <button name="">/</button>
-            <button name="">^</button>
-            <button name="">Clear</button>
-            </div>
-            <br/>
-            <div className="row2">
-            <button name="">7</button>
-            <button name="">8</button>
-            <button name="">9</button>
-            <button name="">Back Space</button>
-            </div>
-            <br/>
-            <div className="row3">
-            <button name="">4</button>
-            <button name="">5</button>
-            <button name="">6</button>
-            <button name="">+</button>
-            </div>
-            <br/>
-            <div className="row4">
-            <button name="">1</button>
-            <button name="">2</button>
-            <button name="">3</button>
-            <button name="">-</button>
-            </div>
-            <br/>
-            <div className="row5">
-            <button>0</button>
-            <button>.</button>
-            <button>00</button>
-            <button>Enter</button>
-            </div>
+
+
+    render() {
+        return (
+            <div className="full-calc">
+                <br />
+                <Screen display={this.props.display} />
+                <div className="btn-grid">
+                    <div className="row1">
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="(">(</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name=")">)</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="*">*</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="/">/</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="^">^</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="clear">Clear</button>
+                    </div>
+                    <br />
+                    <div className="row2">
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="7">7</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="8">8</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="9">9</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="back-space">Back Space</button>
+                    </div>
+                    <br />
+                    <div className="row3">
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="4">4</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="5">5</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="6">6</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="+">+</button>
+                    </div>
+                    <br />
+                    <div className="row4">
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="1">1</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="2">2</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="3">3</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="-">-</button>
+                    </div>
+                    <br />
+                    <div className="row5">
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="0">0</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name=".">.</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="00">00</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="enter">Enter</button>
+                    </div>
+                </div>
             </div>
         )
     }
-       
-            
+
+
 }
--- src/components/CalculatorPage.js
+++ src/components/CalculatorPage.js
@@ -11,11 +11,56 @@ export default class CalculatorPage extends React.Component{
         }
     }
 
+    //decides what function to execute based on the button pressed
+    handleClick = (button) => {
+        switch (button){
+            case "enter":
+                this.calculate()
+                break;
+            case "clear":
+                this.clear()
+                break;
+            case "back-space":
+                this.backSpace()
+                break;
+            default: this.setState({
+                display: this.state.display + button
+            })
+        }
+    }
+
+    //clears the display 
+    clear = () => {
+        this.setState({
+            display: ""
+        })
+    }
+    //removes the last entered number/symbol
+    backSpace = () => {
+        this.setState({
+            display: this.state.display.slice(0,-1)
+        })
+    }
+    //calculate based on the operand 
+    calculate = (button) => {
+        try{
+            this.setState({
+                display: (eval(this.state.display) || "" ) + ""
+            })
+        } 
+        catch(e){
+            this.setState({
+                display: "ERROR"
+            })
+            setTimeout(() => this.clear(), 1000);
+        }
+    }
+
     render(){
         return(
             <div>
                 Calculator
-                <Calculator display={this.state.display}/>
+                <Calculator handleClick={this.handleClick} display={this.state.display}/>
             </div>
         )
     }
