7b962eb932712db5bc80c72d56e6e875b8369bdc
AidanMcB <aidankmcbride@gmail.com>
2020-08-28 22:23:21 -0400

    buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,8 +8,43 @@ export default class Calculator extends React.Component{
     render(){
         console.log(this.props)
         return(
-            <div>0
+            <div>
+            <br />
             <Screen display={this.props.display}/>
+            <div class="row1">
+            <button>*</button>
+            <button>/</button>
+            <button>^</button>
+            <button>Clear</button>
+            </div>
+            <br/>
+            <div class="row2">
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>Back Space</button>
+            </div>
+            <br/>
+            <div class="row3">
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            </div>
+            <br/>
+            <div class="row4">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            </div>
+            <br/>
+            <div class="row5">
+            <button>0</button>
+            <button>.</button>
+            <button>00</button>
+            <button>Enter</button>
+            </div>
             </div>
         )
     }
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,7 +4,7 @@ export default class Screen extends React.Component{
 
     render(){
         return(
-            <div>hello there {this.props.display}</div>
+            <div>{this.props.display}</div>
 
         )
     }
