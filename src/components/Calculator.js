7b203107f67d8587709f0d61e546a9b3bbd596f2
AidanMcB <aidankmcbride@gmail.com>
2020-08-29 18:51:30 -0400

    before trying the grid as a table

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,35 +13,31 @@ export default class Calculator extends React.Component {
                 <Screen display={this.props.display} />
                 <div className="btn-grid">
                     <div className="row1">
-                        <button onClick={(e) => this.props.handleClick(e.target.name) } name="(">(</button>
-                        <button onClick={(e) => this.props.handleClick(e.target.name) } name=")">)</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } className="open-par" name="(">(</button>
+                        <button onClick={(e) => this.props.handleClick(e.target.name) } className="closed-par" name=")">)</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="*">*</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="/">/</button>
                         {/* <button onClick={(e) => this.props.handleClick(e.target.name) } name="^">^</button> */}
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="clear">Clear</button>
                     </div>
-                    <br />
                     <div className="row2">
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="7">7</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="8">8</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="9">9</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="back-space">Back Space</button>
                     </div>
-                    <br />
                     <div className="row3">
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="4">4</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="5">5</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="6">6</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="+">+</button>
                     </div>
-                    <br />
                     <div className="row4">
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="1">1</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="2">2</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="3">3</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="-">-</button>
                     </div>
-                    <br />
                     <div className="row5">
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name="0">0</button>
                         <button onClick={(e) => this.props.handleClick(e.target.name) } name=".">.</button>
